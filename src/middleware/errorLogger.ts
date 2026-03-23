c73edf826f1394a434cc8cea174529a942e513a2
Denalo <Jsimmons561@outlook.com>
2026-03-23 12:51:33 -0400

    added comments to errorlogger

--- src/middleware/errorLogger.ts
+++ src/middleware/errorLogger.ts
@@ -1,16 +1,20 @@
+//import express types with error handling
 import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
-
+//divider for console output
 const divider = "─".repeat(50);
 
+//error handling middleware
 const errorLogger: ErrorRequestHandler = (
   err: any,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  //retrieve status code
   const status = err.status ?? err.statusCode ?? 500;
+  //get timestamp
   const timestamp = new Date().toISOString();
-
+//error log output
   console.error(`
 [EML] ERROR
 ${divider}
@@ -22,7 +26,7 @@ ${divider}
   Stack:     ${err.stack}
 ${divider}`);
 
-  next(err);
+//calls next middleware  next(err);
 };
 
 export default errorLogger;
